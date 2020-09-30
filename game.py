bb298bdd8302ff5618b88b51119923464585b6c9
Deadshot96 <72140485+Deadshot96@users.noreply.github.com>
2020-09-30 21:10:01 +0530

    add Spot functions

--- game.py
+++ game.py
@@ -24,4 +24,55 @@ class Spot:
     self.images = None
     self.neighbours = 0
     self.isDetonated = False
+    
+  def make_mine(self):
+    self.isMine = True
+        
+  def is_mine(self):
+    return self.isMine
 
+  def get_row(self):
+    return self.row
+
+  def get_col(self):
+    return self.col
+
+  def get_dims(self):
+    return self.row, self.col
+
+  def is_reveled(self):
+    return self.reveled
+
+  def is_empty(self):
+    return self.neighbours == 0
+
+  def reveal(self):
+    self.reveled = True
+    self.untag()
+    self.color = GREY_LINE
+
+  def get_neighbours(self):
+    return self.neighbours
+
+  def is_tagged(self):
+    return self.isTagged
+
+  def tag(self):
+    self.isTagged = True
+
+  def untag(self):
+    self.isTagged = False
+
+  def set_neighbours(self, neighbours):
+    self.neighbours = neighbours
+
+  def inc_neighbours(self):
+    self.neighbours += 1
+
+  def detonate(self):
+    self.isDetonated = True
+
+  def is_detonated(self):
+    return self.isDetonated
+
+   
