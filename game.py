1d367be8f097555098dac88ff7d0d439dc0724e7
Deadshot96 <72140485+Deadshot96@users.noreply.github.com>
2020-09-30 21:02:15 +0530

    add colors

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
 import pygame, os, random
+
+# COLORS
+BLACK = (20, 20, 20)
+WHITE = (250, 250, 250)
+GREY = (170, 170, 170)
+GREY_LINE = (100, 100, 100)
+RED = (240, 20, 20)
+GREEN = (20, 240, 20)
+BLUE = (20, 20, 240)
+YELLOW = (240, 240, 20)
